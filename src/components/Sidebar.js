db717e28d01c437e5ad27989fa4a5eae653bb0d7
Lily Go <lilygo7192@gmail.com>
2023-10-02 23:25:17 -0700

    Fix sidebar font size and spacing

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import classNames from "classnames";
-import "./Sidebar.scss";
 
 export default function Sidebar() {
   return (
@@ -8,25 +6,25 @@ export default function Sidebar() {
       <ol>
         <li>
           <div className="step">
-            <span>STEP 1</span>
+            <span className="step-number">STEP 1</span>
             <span className="step-description">YOUR INFO</span>
           </div>        
         </li>
         <li>
           <div className="step">
-            <span>STEP 2</span>
+            <span className="step-number">STEP 2</span>
             <span className="step-description">SELECT PLAN</span>
           </div>
         </li>
         <li>
           <div className="step">
-            <span>STEP 3</span>
+            <span className="step-number">STEP 3</span>
             <span className="step-description">ADD-ONS</span>
           </div>
         </li>
         <li>
           <div className="step">
-            <span>STEP 4</span>
+            <span className="step-number">STEP 4</span>
             <span className="step-description">SUMMARY</span>
           </div>
         </li>
