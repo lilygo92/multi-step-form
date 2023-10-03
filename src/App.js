8e588cf5411ba11440c21422413f0dd270e29af0
Lily Go <lilygo7192@gmail.com>
2023-10-02 19:21:12 -0700

    Add fonts to stylesheet and organize components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Sidebar from './components/Sidebar';
 import PersonalInfo from './components/forms/PersonalInfo';
 import Description from './components/forms/Description';
@@ -8,16 +7,16 @@ import Button from './components/forms/Button';
 function App() {
   return (
     <div className="container">
-        <Sidebar />
+      <Sidebar />
 
-        <div className="form-section">
-          <div id="personal-info">
-            <Description/>
-            <PersonalInfo/>
-            <Button/>  
-          </div>
+      <div className="form-section">
+        <div id="personal-info">
+          <Description/>
+          <PersonalInfo/>
+          <Button/>  
         </div>
       </div>
+    </div>
   );
 }
 
--- src/components/forms/Description.js
+++ src/components/forms/Description.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Description() {
   return (
-    <div>
+    <>
       <h1>Personal info</h1>
       <p>Please provide your name, email address, and phone number.</p>
-    </div>  
+    </>  
   )
 }
