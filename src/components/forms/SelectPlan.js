16070c40fffae8b9bb4979f13c85934982bd788e
Lily Go <lilygo7192@gmail.com>
2023-10-02 20:05:33 -0700

    Create jsx template for step 2 (select plan)

---
+++ src/components/forms/SelectPlan.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function SelectPlan() {
+  return (
+    <div className="form select--plan">
+      <div className="plan"> 
+        <span className="plan-name"></span>
+        <span className="plan-price"></span>
+      </div>
+      <div className="plan"> 
+        <span className="plan-name"></span>
+        <span className="plan-price"></span>
+      </div>
+      <div className="plan"> 
+        <span className="plan-name"></span>
+        <span className="plan-price"></span>
+      </div>
+
+      <div className="plan-toggle">
+        <span></span>
+        
+        <span></span>
+      </div>
+    </div>
+  )
+} 
